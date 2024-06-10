9dd07d7a195fc3537331f644195c89c7a2304ba0
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-06-11 00:45:01 +0100

    Make more responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { primary, secondary } from "@/themes/customs/palette";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-60px)] flex-col items-start md:justify-center justify-start w-full md:mt-[60px] -mt-[24px]">
+    <main className="flex min-h-[calc(100vh-60px)] flex-col md:items-start md:justify-center items-center justify-start w-full md:mt-[60px] -mt-[24px]">
       <div className="flex h-[calc(100vh-60px)] items-center md:justify-start justify-center container ">
         <div className="flex flex-col md:items-start justify-center md:gap-y-2 gap-y-3 lg:px-32 md:px-20 px-5 -mt-5 items-center md:text-start text-center ">
           <div className="uppercase md:text-md text-sm text-[var(--darkBrown)] opacity-50">
@@ -61,7 +61,7 @@ export default function Home() {
           alt=""
           className="md:absolute md:block hidden xl:h-screen lg:h-[80vh] h-[70vh] w-auto right-0 -z-10 lg:pr-20 pr-8"
         />
-        <div className="md:hidden absolute -bottom-28 w-screen h-[50vh] -z-10 bg-right-top bg-cover bg-[url('https://raw.githubusercontent.com/Coffee-Culture-UK/coffee-culture-frontend/main/public/small-hero.png')]"></div>
+        <div className="md:hidden absolute -bottom-96 w-screen h-[90vh] -z-10 bg-top bg-no-repeat bg-contain bg-[url('https://raw.githubusercontent.com/Coffee-Culture-UK/coffee-culture-frontend/main/public/small-hero.png')]"></div>
       </div>
       <div className="h-screen w-full"></div>
     </main>
