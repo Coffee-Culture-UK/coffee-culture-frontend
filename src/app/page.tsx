685d59fc4d98f0ff2b29c6683bc3c765a929ed1b
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-06-11 03:54:20 +0100

    Try fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 import Login from "@/components/auth/loginModal";
 import Register from "@/components/auth/registerModal";
 import Hero from "@/components/hero/hero";
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function Home() {
   const searchParams = useSearchParams();
-    const register = searchParams.get('register') == 'true'
-    const login = searchParams.get('login') == 'true';
-  
-  return (
-    <main className="flex min-h-[calc(100vh-60px)] flex-col md:items-start md:justify-center items-center justify-start w-full md:mt-[60px] ">
-      <Suspense>
-      {register && <Register register={register}/>}
-      {login && <Login login={login} />}
-      <Hero/>
-      </Suspense>
+  const register = searchParams.get("register") == "true";
+  const login = searchParams.get("login") == "true";
 
-      <div className="h-screen w-full"></div>
-    </main>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="flex min-h-[calc(100vh-60px)] flex-col md:items-start md:justify-center items-center justify-start w-full md:mt-[60px] ">
+        {register && <Register register={register} />}
+        {login && <Login login={login} />}
+        <Hero />
+
+        <div className="h-screen w-full"></div>
+      </main>
+    </Suspense>
   );
 }
