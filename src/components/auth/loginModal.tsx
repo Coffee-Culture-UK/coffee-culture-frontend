b6e0ccea27927d42309d2c51be4cd85552de45ba
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-06-11 03:27:41 +0100

    Fix modal onclose

--- src/components/auth/loginModal.tsx
+++ src/components/auth/loginModal.tsx
@@ -1,9 +1,13 @@
 import { Dialog } from "@mui/material";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 export default function Login({login}:{login:boolean}) {
+    const router = useRouter();
+    const pathname = usePathname();
+    
     return(
-        <Dialog open={login}>
+        <Dialog open={login} onClose={()=>{router.push(pathname)}}>
 
         </Dialog>
     )
--- src/components/auth/registerModal.tsx
+++ src/components/auth/registerModal.tsx
@@ -1,9 +1,12 @@
 import { Dialog } from "@mui/material";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 export default function Register({register}:{register:boolean}) {
+    const router = useRouter();
+    const pathname = usePathname();
     return(
-        <Dialog open={register}>
+        <Dialog open={register} onClose={()=>{router.push(pathname)}}>
         </Dialog>
     )
 }
