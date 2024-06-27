fe7d64ff902c6a37b2c0b9034fa38e999e22676b
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-06-27 18:34:15 +0100

    Fix build

--- src/app/register/nav/nav.tsx
+++ src/app/register/nav/nav.tsx
@@ -5,7 +5,7 @@ export default function RegisterNav({step}:{step: string}) {
         <div className="flex gap-x-20">
           {navItems.map((item: NavItem, ref) => {
             return (
-              <div className="flex items-start justify-center w-10">
+              <div key={ref} className="flex items-start justify-center w-10">
                 <div className="flex flex-col items-center justify-center gap-y-2">
                   <div
                     className={` relative w-10 h-10 rounded-full flex items-center justify-center text-[var(--backgroundColour)] text-base font-medium ${
